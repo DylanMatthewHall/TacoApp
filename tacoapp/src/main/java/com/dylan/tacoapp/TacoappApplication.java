7fa90788b3be4d02579e21cf4f423146e901c964
DylanMatthewHall <dylanmatthewhall@outlook.com>
2025-09-12 13:23:02 -0500

    TacoApp Created

---
+++ tacoapp/src/main/java/com/dylan/tacoapp/TacoappApplication.java
@@ -0,0 +1,13 @@
+package com.dylan.tacoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoappApplication.class, args);
+	}
+
+}
---
+++ tacoapp/src/test/java/com/dylan/tacoapp/TacoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.tacoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
